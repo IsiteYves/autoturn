560786994ced40cf17c9f1ebdda80d4b9cbd3693
IsiteYves <yvesisite@gmail.com>
2023-02-25 10:43:15 +0200

    feat: upgraded to working esp8266 code

--- remote_switch/remote_switch.ino
+++ remote_switch/remote_switch.ino
@@ -1,17 +1,15 @@
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPClient.h>
-WiFiClient wifiClient;
+#include<ESP8266WiFi.h>
+WiFiClient http;
 String ssid = "RCA-WiFii";
 String password = "@rca@2023";
 
 int relayPin = D7;
-ESP8266WebServer server;
 
 //Light sensor is connected to the pin A0 (Analog 0)
-int lightSensorPin = A0;
-int lightIntensity;
+//int lightSensorPin = A0;
+//int lightIntensity;
 
-String payload = "{\"deviceip\":";
+String payload = "deviceip=";
 
 void turnOnOrOff(String request) {
   Serial.println(request);
@@ -48,29 +46,23 @@ void setup() {
   Serial.println("Connected to WiFi");
   Serial.print("IP: ");
   Serial.print(WiFi.localIP());
-  payload += "\"" + WiFi.localIP().toString() + "\"}";
-  server.begin();
+  payload += WiFi.localIP().toString();
+  Serial.println("payload: " + payload);
 }
 
 void loop() {
-  HTTPClient http;
-  http.begin(wifiClient, "http://iot.benax.rw/projects/b9a31af7237a309d8f2654f837fd2871/autoturn/utils/check_light_status.php");
-  // http.addHeader("Content-Type", "application/json");  // set the content type
-  int httpCode = http.GET();
-//  if (httpCode > 0) {
-    String response = http.getString();
-    Serial.println("successresponse..." + response+","+(String)httpCode);
-//  }
-//  else {
-//    Serial.println("Error on HTTP request");
-//  }
-  http.end();
-  delay(500);
-  //  lightIntensity = analogRead(lightSensorPin);
-  // Serial.println("light..." + (String) lightIntensity);
-  //  if (lightIntensity < 15) {
-  //    digitalWrite(relayPin, HIGH);
-  //  } else {
-  //    digitalWrite(relayPin, LOW);
-  //  }
+  http.connect("iot.benax.rw", 80);
+  http.println("POST /projects/b9a31af7237a309d8f2654f837fd2871/autoturn/utils/check_light_status.php HTTP/1.1");
+  http.println("Host: iot.benax.rw");
+  http.println("User-Agent: ESP8266/1.0");
+  http.println("Content-Type: application/json");
+  http.println("Content-Length: " + (String)payload.length());
+  http.println();
+  http.print(payload);
+  String response = "";
+  response += http.readStringUntil('\r');
+  String payload2 = http.readString();
+  Serial.println("response: " + payload2);
+  turnOnOrOff(payload2);
+  delay(2000);
 }
